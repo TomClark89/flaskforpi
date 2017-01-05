c17ac78d050de93990a10699b7b31b917ed6bd70
Tom Clark <tom.wil.clark@gmail.com>
2017-01-05 19:24:31 +0000

    added retrieve one task by ID

--- flaskapp.py
+++ flaskapp.py
@@ -22,5 +22,15 @@ tasks = [
 def get_tasks():
     return jsonify({'tasks': tasks})
 
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(400)
+    return jsonify({'task': task[0]})
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True, host= '0.0.0.0')
