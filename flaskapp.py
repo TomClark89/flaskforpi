cfb5dbbebb6965ed274e45368398d4993d1aa37c
Tom Clark <tom.wil.clark@gmail.com>
2017-01-05 21:46:55 +0000

    just put some comments in to the code

--- flaskapp.py
+++ flaskapp.py
@@ -18,10 +18,13 @@ tasks = [
     }
 ]
 
+
+#get all tasks method
 @app.route('/todo/api/v1.0/tasks', methods=['GET'])
 def get_tasks():
     return jsonify({'tasks': tasks})
 
+#get individual task - abort if task ID not found
 from flask import abort
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
@@ -30,11 +33,19 @@ def get_task(task_id):
         abort(404)
     return jsonify({'task': task[0]})
 
-
+#what happens if error code 404 encountered
+#the (error) in the function seems to be meaningless
 from flask import make_response
 @app.errorhandler(404)
 def not_found(error):
+    #this makes a response with some json content saying the error type is not found
+    #the ,404 just feeds in the status code passed with the response (good practice to
+    #match the @errorhandler but not required)
     return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+#this is where i'm going to put the first POST request job
+
 
 
 if __name__ == '__main__':
